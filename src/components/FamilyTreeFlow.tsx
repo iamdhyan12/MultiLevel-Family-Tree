dad727c7e609d98168adb8d768d3812d3aa08433
iamdhyan12 <pdhyan92@gmail.com>
2025-11-29 13:14:40 -0500

    Added react-flow2

--- src/components/FamilyTreeFlow.tsx
+++ src/components/FamilyTreeFlow.tsx
@@ -14,6 +14,11 @@ import familyData from '../data/family.json';
 // ============================================
 // TYPES
 // ============================================
+
+
+
+
+
 interface FamilyMember {
   id: string;
   name: string;
